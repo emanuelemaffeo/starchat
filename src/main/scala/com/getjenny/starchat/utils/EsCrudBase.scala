47ebb6ce19ccce8f204c82bc43966bab6c3ab586
emaffeo <emanuele@getjenny.com>
2019-10-21 17:55:27 +0200

    crud generalization

---
+++ src/main/scala/com/getjenny/starchat/utils/EsCrudBase.scala
@@ -0,0 +1,69 @@
+package com.getjenny.starchat.utils
+
+import com.getjenny.starchat.services.DtReloadService.elasticClient
+import com.getjenny.starchat.services.TermService.elasticClient
+import com.getjenny.starchat.services.esclient.ElasticClient
+import org.elasticsearch.action.search.{SearchRequest, SearchResponse, SearchType}
+import org.elasticsearch.action.update.{UpdateRequest, UpdateResponse}
+import org.elasticsearch.client.RequestOptions
+import org.elasticsearch.common.unit.TimeValue
+import org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder
+import org.elasticsearch.index.query.{BoolQueryBuilder, QueryBuilder, QueryBuilders}
+import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.search.sort.SortOrder
+
+class EsCrudBase(client: ElasticClient, index: String) {
+
+  val completeIndexName = Index.indexName(index, client.indexSuffix)
+
+  def find(instance: String,
+           query: BoolQueryBuilder,
+           sortBy: String,
+           sortOrder: SortOrder = SortOrder.ASC,
+           maxItems: Option[Long] = None,
+           searchType: SearchType = SearchType.DEFAULT): SearchResponse = {
+
+    val finalQuery = query.filter(QueryBuilders.termsQuery("instance", instance))
+
+    val sourceReq: SearchSourceBuilder = new SearchSourceBuilder()
+      .query(finalQuery)
+      .size(maxItems.getOrElse(100L).toInt)
+      .version(true)
+      .sort(sortBy, sortOrder)
+
+    val searchReq = new SearchRequest(completeIndexName)
+      .source(sourceReq)
+      .scroll(new TimeValue(60000))
+      .searchType(searchType)
+
+    client.httpClient.search(searchReq, RequestOptions.DEFAULT)
+  }
+
+  def update(instance: String, id: String, fields: Map[String, String]): UpdateResponse = {
+    val builder = jsonBuilder().startObject()
+    fields.foreach { case (k, v) => builder.field(k, v) }
+    builder.field("instance", instance)
+    .endObject()
+
+
+    import org.elasticsearch.common.Strings
+    println(s"Insert: ${Strings.toString(builder)}")
+
+    val updateReq = new UpdateRequest()
+      .index(completeIndexName)
+      .doc(builder)
+      .id(id)
+      .docAsUpsert(true)
+
+    client.httpClient.update(updateReq, RequestOptions.DEFAULT)
+  }
+
+  def deleteById(instance: String, id: String): Unit = {
+
+  }
+
+  def deleteByQuery(instance: String, query: String): Unit = {
+
+  }
+
+}
---
+++ src/test/scala/com/getjenny/starchat/resources/EsCrudBaseTest.scala
@@ -0,0 +1,69 @@
+package com.getjenny.starchat.resources
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import com.getjenny.starchat.serializers.JsonSupport
+import com.getjenny.starchat.services.esclient.IndexManagementElasticClient
+import com.getjenny.starchat.utils.EsCrudBase
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest
+import org.elasticsearch.action.search.SearchRequest
+import org.elasticsearch.action.support.master.AcknowledgedResponse
+import org.elasticsearch.client.RequestOptions
+import org.elasticsearch.client.indices.CreateIndexRequest
+import org.elasticsearch.common.settings.Settings
+import org.elasticsearch.common.unit.TimeValue
+import org.elasticsearch.common.xcontent.XContentType
+import org.elasticsearch.index.query.{BoolQueryBuilder, QueryBuilders}
+import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
+import scala.collection.JavaConverters._
+
+class EsCrudBaseTest extends FunSuite with Matchers with ScalatestRouteTest with JsonSupport with BeforeAndAfterAll {
+
+  val client = IndexManagementElasticClient
+
+  val indexName = "test_index"
+
+  val esCrudBase = new EsCrudBase(client, indexName)
+
+  override protected def beforeAll(): Unit = {
+    val request = new CreateIndexRequest(indexName)
+    request.settings(Settings.builder()
+      .put("index.number_of_shards", 1)
+      .put("index.number_of_replicas", 1)
+    );
+    request.mapping(
+      """{
+        "properties": {
+            "message": {"type": "keyword"},
+            "instance": { "type": "keyword"}
+            }
+      }""",
+      XContentType.JSON);
+    import org.elasticsearch.client.RequestOptions
+    client.httpClient.indices.create(request, RequestOptions.DEFAULT)
+  }
+
+  test("upsert test") {
+    val res = esCrudBase.update("prova", "asasd3f", Map("message" -> "ciao"))
+
+    println(res.status())
+  }
+
+  test("find test") {
+    Thread.sleep(1000)
+
+    val boolQueryBuilder2: BoolQueryBuilder = QueryBuilders.boolQuery()
+      .filter(QueryBuilders.termQuery("message", "ciao"))
+
+    val res = esCrudBase.find("prova", boolQueryBuilder2, "message")
+
+    res.getHits.forEach(println)
+  }
+
+  override protected def afterAll(): Unit = {
+     val deleteIndexReq = new DeleteIndexRequest(indexName)
+
+     client.httpClient.indices.delete(deleteIndexReq, RequestOptions.DEFAULT)
+   }
+
+}
