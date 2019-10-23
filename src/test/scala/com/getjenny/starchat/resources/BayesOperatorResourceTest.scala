2cc606c66ab57d0e73b47849877df0728a11936e
Andrea Collamati <andrea@getjenny.com>
2019-10-23 21:14:16 +0200

    Added other non-regression tests for Bayes Operator

--- src/test/scala/com/getjenny/starchat/resources/BayesOperatorResourceTest.scala
+++ src/test/scala/com/getjenny/starchat/resources/BayesOperatorResourceTest.scala
@@ -1,10 +1,11 @@
 package com.getjenny.starchat.resources
 
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, Multipart, StatusCodes}
+import akka.http.scaladsl.model.{StatusCodes}
+import com.getjenny.analyzer.expressions.{AnalyzersData}
 import com.getjenny.starchat.entities._
-import com.getjenny.starchat.utils.Index
 
 class BayesOperatorResourceTest extends TestEnglishBase {
+
 
   "StarChat" should {
     "return an HTTP code 201 when creating a new user" in {
@@ -22,21 +23,37 @@ class BayesOperatorResourceTest extends TestEnglishBase {
 
 
   /* To test the Bayes Operator we will the following two states
-      S1:  Analyzer Condition = keyword("Keyword1")
+      S1:  Analyzer Condition = bayes(keyword("Keyword1"))
            WQ = Keyword1 is good
            WQ = Keyword1 is wonderful
            WQ = Keyword2 is so so
-      S2:  Analyzer Condition = keyword("Keyword2")
+      S2:  Analyzer Condition = bayes(keyword("Keyword2"))
            WQ = Keyword1 is enough
            WQ = Keyword2 is quite good
 
-      S1: BayesOperator result if applied on "I love Keyword1" = 2/3
-      S2: BayesOperator result if applied on "I love Keyword1" = 0
+      S3:  Analyzer Condition = bayes(keyword("Keyword3"))
+           No WQ
+
+
+
+      User Query: "I love Keyword1"
+      S1: BayesOperator result  = 2/3
+      S2: BayesOperator result  = 0
+      S3: BayesOperator result  = 0
       Winner State is S1 with score 0.6666
 
-      S1: BayesOperator result if applied on "I love Keyword2" = 0
-      S2: BayesOperator result if applied on "I love Keyword2" = 1/2
+      User Query: "I love Keyword2"
+      S1: BayesOperator result  = 0
+      S2: BayesOperator result  = 1/2
+      S3: BayesOperator result  = 0
       Winner State is S2 with score 0.5000
+
+      User Query: "I love Keyword3"
+      S1: BayesOperator result  = 0
+      S2: BayesOperator result  = 0
+      S3: BayesOperator result  = 1.0
+      Winner State is S3 with score 1.0
+
    */
 
   it should {
@@ -55,7 +72,7 @@ class BayesOperatorResourceTest extends TestEnglishBase {
         maxStateCount = 0,
         analyzer = "bayes(keyword(\"Keyword1\"))",
         queries = List("Keyword1 is good", "Keyword1 is wonderful", "Keyword2 is so so"),
-        bubble = "Keyword1 is the winner",
+        bubble = "S1 is the winner",
         action = "",
         actionInput = Map(),
         stateData = Map(),
@@ -71,7 +88,23 @@ class BayesOperatorResourceTest extends TestEnglishBase {
         maxStateCount = 0,
         analyzer = "bayes(keyword(\"Keyword2\"))",
         queries = List("Keyword1 is enough", "Keyword2 is quite good"),
-        bubble = "Keyword2 is the winner",
+        bubble = "S2 is the winner",
+        action = "",
+        actionInput = Map(),
+        stateData = Map(),
+        successValue = "",
+        failureValue = "",
+        evaluationClass = Some("default"),
+        version = None
+      )
+
+      val decisionTableRequest3 = DTDocument(
+        state = "S3",
+        executionOrder = 0,
+        maxStateCount = 0,
+        analyzer = "bayes(keyword(\"Keyword3\"))",
+        queries = List(),
+        bubble = "S3 is the winner",
         action = "",
         actionInput = Map(),
         stateData = Map(),
@@ -94,6 +127,14 @@ class BayesOperatorResourceTest extends TestEnglishBase {
         val response = responseAs[IndexDocumentResult]
         response.created should be(true)
         response.id should be("S2")
+        response.index should be("index_getjenny_english_0.state")
+        response.version should be(1)
+      }
+      Post(s"/index_getjenny_english_0/decisiontable?refresh=1", decisionTableRequest3) ~> addCredentials(testUserCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.Created
+        val response = responseAs[IndexDocumentResult]
+        response.created should be(true)
+        response.id should be("S3")
         response.index should be("index_getjenny_english_0.state")
         response.version should be(1)
       }
@@ -161,6 +202,69 @@ class BayesOperatorResourceTest extends TestEnglishBase {
         response.map(_.score) should contain only (1.0 / 2.0)
       }
 
+    }
+  }
+
+  it should {
+    "return S3 with score 1.0 when get_next_response input is [I love Keyword3]" in {
+      val request = ResponseRequestIn(
+        conversationId = "conv_12345",
+        traversedStates = None,
+        userInput = Some(ResponseRequestInUserInput(
+          text = Some("I love Keyword3"),
+          img = None
+        )),
+        state = None,
+        data = None,
+        threshold = Some(0),
+        evaluationClass = None,
+        maxResults = Some(1),
+        searchAlgorithm = Some(SearchAlgorithm.NGRAM2)
+      )
+
+
+      Post("/index_getjenny_english_0/get_next_response", request) ~> addCredentials(testUserCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.OK
+        val response = responseAs[List[ResponseRequestOut]]
+        response.map(_.state) should contain only ("S3")
+        response.map(_.score) should contain only (1.0)
+      }
+
+    }
+  }
+
+  /* Unary Operator Test */
+  "Bayes operator" should {
+    "return an HTTP code 400 when evaluating a bayes operator analyzer with no argument" in {
+      val evaluateRequest: AnalyzerEvaluateRequest =
+        AnalyzerEvaluateRequest(
+          query = "user query",
+          analyzer = """bayes()""",
+          data = Option {
+            AnalyzersData()
+          }
+        )
+
+      Post(s"/index_getjenny_english_0/analyzer/playground", evaluateRequest) ~> addCredentials(testUserCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.BadRequest
+      }
+    }
+  }
+
+  it should {
+    "return an HTTP code 400 when evaluating a bayes operator analyzer with more than one argument" in {
+      val evaluateRequest: AnalyzerEvaluateRequest =
+        AnalyzerEvaluateRequest(
+          query = "user query",
+          analyzer = """bayes(vOneKeyword("test1"),vOneKeyword("test2"))""",
+          data = Option {
+            AnalyzersData()
+          }
+        )
+
+      Post(s"/index_getjenny_english_0/analyzer/playground", evaluateRequest) ~> addCredentials(testUserCredentials) ~> routes ~> check {
+        status shouldEqual StatusCodes.BadRequest
+      }
     }
   }
 
