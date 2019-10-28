af3fa9291e77f8ee50fce69b31bdaab3073b4f4b
emaffeo <emanuele@getjenny.com>
2019-10-28 11:08:52 +0100

    removed some duplicated code

--- src/main/scala/com/getjenny/starchat/services/esclient/EsCrudBase.scala
+++ src/main/scala/com/getjenny/starchat/services/esclient/EsCrudBase.scala
@@ -12,15 +12,14 @@ import org.elasticsearch.action.update.{UpdateRequest, UpdateResponse}
 import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.common.Strings
 import org.elasticsearch.common.unit.TimeValue
-import org.elasticsearch.common.xcontent.{DeprecationHandler, NamedXContentRegistry, ObjectPath, XContentBuilder, XContentParser}
 import org.elasticsearch.common.xcontent.XContentFactory.jsonBuilder
-import org.elasticsearch.index.query.{BoolQueryBuilder, Operator, QueryBuilder, QueryBuilders}
+import org.elasticsearch.common.xcontent._
+import org.elasticsearch.index.query.{QueryBuilder, QueryBuilders}
 import org.elasticsearch.index.reindex.{BulkByScrollResponse, DeleteByQueryRequest}
 import org.elasticsearch.search.aggregations.AggregationBuilder
 import org.elasticsearch.search.builder.SearchSourceBuilder
 import org.elasticsearch.search.sort.SortBuilder
 
-import scala.collection.JavaConverters._
 //private constructor - only the factory object can create an instance,
 // in order to be sure that the index name passed as parameter is formatted correctly,
 class EsCrudBase private(client: ElasticClient, index: String) {
@@ -51,7 +50,7 @@ class EsCrudBase private(client: ElasticClient, index: String) {
            fetchSource: Option[Array[String]] = None): SearchResponse = {
 
     val finalQuery = QueryBuilders.boolQuery()
-      .must(QueryBuilders.matchQuery("instance", instance))
+      .must(QueryBuilders.matchQuery(Instance, instance))
       .must(queryBuilder)
 
     val search: SearchSourceBuilder = new SearchSourceBuilder()
@@ -72,9 +71,9 @@ class EsCrudBase private(client: ElasticClient, index: String) {
     if (scroll) request.scroll(new TimeValue(scrollTime))
     requestCache.map(request.requestCache(_))
 
-    val response = client.httpClient.search(request, RequestOptions.DEFAULT)
-    response.getHits.getHits.map(_.getSourceAsMap).foreach { x => x.asScala.foreach(y => println(s"$x -> $y"))}
-    response
+    log.debug("Search on instance {} - search request: {}", instance, request)
+
+    client.httpClient.search(request, RequestOptions.DEFAULT)
   }
 
   def scroll(scrollRequest: SearchScrollRequest): SearchResponse = {
@@ -82,14 +81,84 @@ class EsCrudBase private(client: ElasticClient, index: String) {
   }
 
   def index(instance: String, id: String, builder: XContentBuilder): IndexResponse = {
+    log.debug("Indexing instance {} - id: {}", instance, id)
+    val request: IndexRequest = createIndexRequest(id, builder)
+    client.httpClient.index(request, RequestOptions.DEFAULT)
+  }
+
+  def index(instance: String, id: String, fields: Map[String, String]): IndexResponse = {
+    val builder: XContentBuilder = builderFromMap(instance, fields)
+    index(instance, id, builder)
+  }
+
+  def bulkInsert(elems: List[(String, XContentBuilder)]): BulkResponse = {
+    val request = new BulkRequest
+    elems.foreach { case (id, builder) => request.add(createIndexRequest(id, builder))}
+
+    client.httpClient.bulk(request, RequestOptions.DEFAULT)
+  }
+
+  def update(instance: String, id: String, builder: XContentBuilder, upsert: Boolean = false): UpdateResponse = {
+    log.debug("Update instance {} - id: {}", instance, id)
+    val request: UpdateRequest = createUpdateRequest(id, builder, upsert)
+    client.httpClient.update(request, RequestOptions.DEFAULT)
+  }
+
+  def update(instance: String, id: String, fields: Map[String, String]): UpdateResponse = {
+    val builder: XContentBuilder = builderFromMap(instance, fields)
+    update(instance, id, builder)
+  }
+
+  def bulkUpdate(elems: List[(String, XContentBuilder)], upsert: Boolean = false): BulkResponse = {
+    val request = new BulkRequest
+    elems.foreach { case (id, builder) => request.add(createUpdateRequest(id, builder, upsert))}
+
+    client.httpClient.bulk(request, RequestOptions.DEFAULT)
+  }
+
+  def deleteByQuery(instance: String, queryBuilder: QueryBuilder): BulkByScrollResponse = {
+    val finalQuery = QueryBuilders.boolQuery()
+      .must(QueryBuilders.matchQuery(Instance, instance))
+      .must(queryBuilder)
+
+    val request = new DeleteByQueryRequest(index)
+    request.setConflicts("proceed")
+    request.setQuery(finalQuery)
+
+    log.debug("Delete instance {} - request: {}", instance, request)
+
+    client.httpClient.deleteByQuery(request, RequestOptions.DEFAULT)
+  }
+
+  def refresh(): RefreshIndexResult = {
+    client.refresh(index)
+  }
+
+  private[this] def builderFromMap(instance: String, fields: Map[String, String]) = {
+    val builder = jsonBuilder().startObject()
+    fields.foreach { case (k, v) => builder.field(k, v) }
+    builder.field(Instance, instance)
+      .endObject()
+    builder
+  }
+
+  private[this] def createIndexRequest(id: String, builder: XContentBuilder): IndexRequest = {
     require(isInstanceEvaluated(builder), "instance field must be present while indexing a new document")
 
-    val request = new IndexRequest()
+    new IndexRequest()
       .index(index)
       .source(builder)
       .id(id)
+  }
 
-    client.httpClient.index(request, RequestOptions.DEFAULT)
+  private[this] def createUpdateRequest(id: String, builder: XContentBuilder, upsert: Boolean): UpdateRequest = {
+    require(isInstanceEvaluated(builder), "instance field must be present while indexing a new document")
+
+    new UpdateRequest()
+      .index(index)
+      .doc(builder)
+      .id(id)
+      .docAsUpsert(upsert)
   }
 
   private[this] def isInstanceEvaluated(builder: XContentBuilder): Boolean = {
@@ -97,87 +166,11 @@ class EsCrudBase private(client: ElasticClient, index: String) {
       .createParser(NamedXContentRegistry.EMPTY,
         DeprecationHandler.THROW_UNSUPPORTED_OPERATION, Strings.toString(builder))
 
-   Option(if (parser.nextToken() == XContentParser.Token.START_ARRAY) {
+    Option(if (parser.nextToken() == XContentParser.Token.START_ARRAY) {
       ObjectPath.eval[String](Instance, parser.listOrderedMap())
     } else {
       ObjectPath.eval[String](Instance, parser.mapOrdered())
     }).nonEmpty
-  }
-
-
-  def index(instance: String, id: String, fields: Map[String, String]): IndexResponse = {
-    val builder = jsonBuilder().startObject()
-    fields.foreach { case (k, v) => builder.field(k, v) }
-    builder.field(Instance, instance)
-      .endObject()
-
-    index(instance, id, builder)
-  }
-
-  def bulkUpdate(elems: List[(String, XContentBuilder)], upsert: Boolean = false): BulkResponse = {
-    val request = new BulkRequest
-    elems.foreach { case (id, builder) =>
-      require(isInstanceEvaluated(builder), "instance field must be present while indexing a new document")
-
-      val updateRequest = new UpdateRequest()
-        .index(index)
-        .docAsUpsert(upsert)
-        .doc(builder)
-        .id(id)
-      request.add(updateRequest)
-    }
-
-    client.httpClient.bulk(request, RequestOptions.DEFAULT)
-  }
-
-  def bulkInsert(elems: List[(String, XContentBuilder)]): BulkResponse = {
-    val request = new BulkRequest
-    elems.foreach { case (id, builder) =>
-      require(isInstanceEvaluated(builder), "instance field must be present while indexing a new document")
-
-      val indexRequest = new IndexRequest()
-        .index(index)
-        .source(builder)
-        .id(id)
-      request.add(indexRequest)
-    }
-
-    client.httpClient.bulk(request, RequestOptions.DEFAULT)
-  }
-
-  def update(instance: String, id: String, builder: XContentBuilder, upsert: Boolean = false): UpdateResponse = {
-    require(isInstanceEvaluated(builder), "instance field must be present while indexing a new document")
-
-    val request = new UpdateRequest()
-      .index(index)
-      .doc(builder)
-      .id(id)
-      .docAsUpsert(upsert)
-
-    client.httpClient.update(request, RequestOptions.DEFAULT)
-  }
-
-  def update(instance: String, id: String, fields: Map[String, String]): UpdateResponse = {
-    val builder = jsonBuilder().startObject()
-    fields.foreach { case (k, v) => builder.field(k, v) }
-    builder.field(Instance, instance)
-      .endObject()
-
-    update(instance, id, builder)
-  }
-
-  def deleteByQuery(instance: String, query: BoolQueryBuilder): BulkByScrollResponse = {
-    query.filter(QueryBuilders.termQuery(Instance, instance))
-
-    val request = new DeleteByQueryRequest(index)
-    request.setConflicts("proceed")
-    request.setQuery(query)
-
-    client.httpClient.deleteByQuery(request, RequestOptions.DEFAULT)
-  }
-
-  def refresh(): RefreshIndexResult = {
-    client.refresh(index)
   }
 
 }
