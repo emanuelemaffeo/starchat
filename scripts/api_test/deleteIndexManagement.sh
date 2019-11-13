b37e7f126d251c05c480250223af228dda612444
emaffeo <emanuele@getjenny.com>
2019-11-13 16:53:48 +0100

    updated scripts

--- scripts/api_test/deleteIndexManagement.sh
+++ scripts/api_test/deleteIndexManagement.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 PORT=${1:-8888}
-INDEX_NAME=${2:-index_english}
+INDEX_NAME=${2:-index_getjenny_english_0}
 INDEX_SUFFIX=${3}
-if [[ ! -z ${INDEX_SUFFIX} ]]; then
-  SUFFIX="&indexSuffix=${INDEX_SUFFIX}"
-fi
 
-curl -v -H "Authorization: Basic $(echo -n 'test_user:p4ssw0rd' | base64)" \
-  -H "Content-Type: application/json" -X DELETE "http://localhost:${PORT}/language_index_management?index_name=${INDEX_NAME}${SUFFIX}"
+curl -v -H "Authorization: Basic $(echo -n 'admin:adminp4ssw0rd' | base64)" \
+  -H "Content-Type: application/json" -X DELETE "http://localhost:${PORT}/${INDEX_NAME}/index_management" -d'{}'
 
